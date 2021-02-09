c0f735362dce25c31f9343cb88e211b381500727
Roxxas96 <Roxxas9676@gmail.com>
2021-02-09 02:27:48 +0100

    init

--- frontend/src/app/models/RoomSimple.model.ts
+++
@@ -1,7 +0,0 @@
-export class RoomSimple {
-  name: String = '';
-  max_players: Number = 0;
-  players: Number = 0;
-  gameInProgress: Number = 0;
-  host: { username: String } = { username: '' };
-}
